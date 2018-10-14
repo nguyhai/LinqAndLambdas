260d88e5a2106aee3ba664c614061dceee16506c
nguyhai <tenth11@gmail.com>
2018-10-14 18:43:19 -0500

    Split up conditions and using orderby

--- LinqAndLambdas/Program.cs
+++ LinqAndLambdas/Program.cs
@@ -16,9 +16,22 @@ namespace LinqAndLambdas
             string[] catNames = { "Lucky", "Bella", "Luna", "Orea", "Simba", "Toby", "Loki", "Oscar"};
             int[] numbers = { 1, 2, 5, 3, 5, 2, 5, 7, 87, 2, 3, 4, 35, 6, 1, 1, 7, 5, 2, 4, 54 };
 
+            // Here we are simple creating the getTheNumbers query. It is NOT executed until it is called in the WriteLine below
+
+            // You can also split up the conditions to make it more readable, and also order them by ascending or descending:
             var getTheNumbers = from number in numbers
-                                where number < 5
+                                where (number > 5)
+                                where (number < 10)
+                                orderby number descending
                                 select number;
+
+            var getCatNames = from catName in catNames
+                              where catName.Length == 4
+                              select catName;
+
+            var catsWithA = from cat in catNames
+                            where cat.Contains("a") && (cat.Length < 5)
+                            select cat;
 
             // Making a list of numbers, and adding from numbers array if the number is less than 5
             List<int> newNumbers = new List<int>();
@@ -34,10 +47,8 @@ namespace LinqAndLambdas
             // Printing both to see what they do. The loop does the same thing, but it is much easier to just use a LINQ query for the same result. 
             Console.WriteLine(string.Join(", ", getTheNumbers));
             Console.WriteLine(string.Join(", ", newNumbers));
-
-
-
-
+            Console.WriteLine(string.Join(", ", getCatNames));
+            Console.WriteLine(string.Join(", ", catsWithA));
 
 
 
