055b925ddc0958ddfd27b1509b98ee514694ab16
nguyhai <tenth11@gmail.com>
2018-10-14 00:33:20 -0500

    Simple LINQ query example

--- LinqAndLambdas/Program.cs
+++ LinqAndLambdas/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Linq; // Need to have this namespace if you want to use LINQ
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +12,15 @@ namespace LinqAndLambdas
     {
         static void Main(string[] args)
         {
+            string sentence = "I Love cats";
+            string[] catNames = { "Lucky", "Bella", "Luna", "Orea", "Simba", "Toby", "Loki", "Oscar"};
+            int[] numbers = { 1, 2, 5, 3, 5, 2, 5, 7, 87, 2, 3, 4, 35, 6, 1, 1, 7, 5, 2, 4, 54 };
+
+            var getTheNumbers = from number in numbers
+                                select number; // No where clause, so we are selecting all numbers. 
+
+            Console.WriteLine(string.Join(", ", getTheNumbers));
+
 
         }
     }
