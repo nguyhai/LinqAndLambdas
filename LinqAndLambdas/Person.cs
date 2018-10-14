ab6f9f3137ca9164b134876bf74b8dbb74570f32
nguyhai <tenth11@gmail.com>
2018-10-14 18:58:55 -0500

    Created Person class and Gender enum, added objects to a  list

---
+++ LinqAndLambdas/Person.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqAndLambdas
+{
+    public class Person
+    {
+        private string _name;
+        private int _height;
+        private int _weight;
+
+        private Gender _gender;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return _height;
+            }
+            set
+            {
+                _height = value;
+            }
+        }
+
+        public int Weight
+        {
+            get
+            {
+                return _weight;
+            }
+            set
+            {
+                _weight = value;
+            }
+        }
+
+        public Gender Gender
+        {
+            get
+            {
+                return _gender;
+            }
+            set
+            {
+                _gender = value;
+            }
+        }
+
+        public Person(string name, int height, int weight, Gender gender)
+        {
+            Name = name;
+            Height = height;
+            Weight = weight;
+            Gender = gender;
+        }
+
+
+    }
+}
--- LinqAndLambdas/Program.cs
+++ LinqAndLambdas/Program.cs
@@ -12,46 +12,66 @@ namespace LinqAndLambdas
     {
         static void Main(string[] args)
         {
-            string sentence = "I Love cats";
-            string[] catNames = { "Lucky", "Bella", "Luna", "Orea", "Simba", "Toby", "Loki", "Oscar"};
-            int[] numbers = { 1, 2, 5, 3, 5, 2, 5, 7, 87, 2, 3, 4, 35, 6, 1, 1, 7, 5, 2, 4, 54 };
-
-            // Here we are simple creating the getTheNumbers query. It is NOT executed until it is called in the WriteLine below
-
-            // You can also split up the conditions to make it more readable, and also order them by ascending or descending:
-            var getTheNumbers = from number in numbers
-                                where (number > 5)
-                                where (number < 10)
-                                orderby number descending
-                                select number;
-
-            var getCatNames = from catName in catNames
-                              where catName.Length == 4
-                              select catName;
-
-            var catsWithA = from cat in catNames
-                            where cat.Contains("a") && (cat.Length < 5)
-                            select cat;
-
-            // Making a list of numbers, and adding from numbers array if the number is less than 5
-            List<int> newNumbers = new List<int>();
-
-            foreach (var number in numbers)
+            // LINQ Queries on Objects
+            List<Person> people = new List<Person>()
             {
-                if (number < 5)
-                {
-                    newNumbers.Add(number);
-                }
-            }
-
-            // Printing both to see what they do. The loop does the same thing, but it is much easier to just use a LINQ query for the same result. 
-            Console.WriteLine(string.Join(", ", getTheNumbers));
-            Console.WriteLine(string.Join(", ", newNumbers));
-            Console.WriteLine(string.Join(", ", getCatNames));
-            Console.WriteLine(string.Join(", ", catsWithA));
+                new Person("Hai", 120, 70, Gender.Male),
+                new Person("Fey", 110, 60, Gender.Female),
+                new Person("Kira", 130, 30, Gender.Female),
+                new Person("Luna", 140, 90, Gender.Female),
+                new Person("Anna", 150, 100, Gender.Female),
+                new Person("Kyle", 160, 120, Gender.Male),
+                new Person("John", 170, 77, Gender.Male)
+            };
 
 
 
+
+
+
+
+
+
+
+
+            //----------------------------------------------------------------------------------------------------------------------------------------//
+            //string sentence = "I Love cats";
+            //string[] catNames = { "Lucky", "Bella", "Luna", "Orea", "Simba", "Toby", "Loki", "Oscar"};
+            //int[] numbers = { 1, 2, 5, 3, 5, 2, 5, 7, 87, 2, 3, 4, 35, 6, 1, 1, 7, 5, 2, 4, 54 };
+
+            //// Here we are simple creating the getTheNumbers query. It is NOT executed until it is called in the WriteLine below
+
+            //// You can also split up the conditions to make it more readable, and also order them by ascending or descending:
+            //var getTheNumbers = from number in numbers
+            //                    where (number > 5)
+            //                    where (number < 10)
+            //                    orderby number descending
+            //                    select number;
+
+            //var getCatNames = from catName in catNames
+            //                  where catName.Length == 4
+            //                  select catName;
+
+            //var catsWithA = from cat in catNames
+            //                where cat.Contains("a") && (cat.Length < 5)
+            //                select cat;
+
+            //// Making a list of numbers, and adding from numbers array if the number is less than 5
+            //List<int> newNumbers = new List<int>();
+
+            //foreach (var number in numbers)
+            //{
+            //    if (number < 5)
+            //    {
+            //        newNumbers.Add(number);
+            //    }
+            //}
+
+            //// Printing both to see what they do. The loop does the same thing, but it is much easier to just use a LINQ query for the same result. 
+            //Console.WriteLine(string.Join(", ", getTheNumbers));
+            //Console.WriteLine(string.Join(", ", newNumbers));
+            //Console.WriteLine(string.Join(", ", getCatNames));
+            //Console.WriteLine(string.Join(", ", catsWithA));
 
         }
     }
