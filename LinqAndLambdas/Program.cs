6a067c1fccf4f6cef07fbae6e25b7a0963658652
nguyhai <tenth11@gmail.com>
2018-10-14 00:25:59 -0500

    Add project files.

---
+++ LinqAndLambdas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Practice program to learn more about LINQ and Lambdas
+
+namespace LinqAndLambdas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
